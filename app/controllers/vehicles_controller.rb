639c849554a2c8690d91227153793fb1f6faf7dc
Simon Herout <simon@herout.eu>
2021-08-17 17:27:59 +0200

    Show html done

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,7 +1,17 @@
 class VehiclesController < ApplicationController
+  before_action :set_vehicle, only: [:show]
 
   def index
     @vehicles = Vehicle.all
   end
 
+  def show
+    @renting = Renting.new
+  end
+
+  private
+
+  def set_vehicle
+    @vehicle = Vehicle.find(params[:id])
+  end
 end
