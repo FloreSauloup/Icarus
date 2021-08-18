bbeec6c41a32ef5a9b4fc185ec94380d9de71d48
Simon Herout <simon@herout.eu>
2021-08-18 12:52:40 +0200

    Index and Show for Rentings

---
+++ app/controllers/rentings_controller.rb
@@ -0,0 +1,17 @@
+class RentingsController < ApplicationController
+  before_action :set_renting, only: [:show]
+
+  def index
+    @rentings = Renting.all
+  end
+
+  def show
+    @renting = Renting.new
+  end
+
+  private
+
+  def set_renting
+    @renting = Renting.find(params[:id])
+  end
+end
